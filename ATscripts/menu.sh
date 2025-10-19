ec80264e78bb996532732daa4d181321e1dbec39
qljshyph <165464963+qljsyph@users.noreply.github.com>
2025-10-19 10:06:47 +0800

    1.13.0

--- ATscripts/menu.sh
+++ ATscripts/menu.sh
@@ -2,6 +2,7 @@
 
 BASE_URL="https://ghfast.top/https://raw.githubusercontent.com/qljsyph/ATAsst/refs/heads/main/ATscripts"
 SCRIPTS_DIR="/etc/mihomo/scripts"
+# 版本:1.13.0
 LOCAL_VERSION="1.13.0"
 # 清空并定义关联数组
 unset files
